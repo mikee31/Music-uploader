5ae12cedaa1b3289a24435c6488b7be7842a5bf6
mikee31 <113145019+mikee31@users.noreply.github.com>
2022-10-01 13:19:43 -0400

    now with better url validation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using OpenQA.Selenium.Chrome;
+using System.Net;
 
 namespace MusicUploader
 {
@@ -8,13 +9,13 @@ namespace MusicUploader
         public const string SAVE_FILE = @"\infos.sav";
         public const string DOWNLOAD_FOLDER_NAME = "tempDownloads";
         public const string CHANGE_URL_MESSAGE = "Do you want to change the playlist url? (y/n)";
-        public const string URL_PROMPT = "Enter the adress of the playlist that you want to upload.";
+        public const string URL_PROMPT = "Enter the adress of the playlist you want to upload.";
         public const string IP_PROMPT = "Enter the ip adress where songs will be uploaded.";
         public const string FIRST_VID_TO_UPLOAD_PROMPT = "Which video is the first one you want to upload?";
         public static readonly string FFMPEG_PATH = Environment.CurrentDirectory + @"\ffmpeg.exe"; // to determine.
         public static readonly string DOWNLOAD_FOLDER_PATH = Environment.CurrentDirectory + @"\" + DOWNLOAD_FOLDER_NAME + @"\";
+        public static readonly string VALIDATOR_URL = "https://www.youtube.com/oembed?format=json&url=";
         public static readonly Regex IP_REGEX = new Regex(@"^([0-9]{1,3}\.){3}[0-9]{1,3}$");
-        public static readonly Regex URL_REGEX = new Regex(@"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$");
         public static ManualResetEvent resetEventStartUpload = new ManualResetEvent(false);
         public static ManualResetEvent resetEventUpload = new ManualResetEvent(false);
         public static ManualResetEvent resetEventDownload = new ManualResetEvent(false);
@@ -22,6 +23,7 @@ namespace MusicUploader
         /****************************************************************** TODO *******************************************************************/
         /* 
         * - A LOT and I mean a whole fucking lot of validation and exceptions.
+        * 
         * - Save infos in json file.
         * - Change validation of youtube urls to whats in the tests project (use validation website instead of regex).
         * - Make it possible to upload video instead of whole playlist.
@@ -29,6 +31,8 @@ namespace MusicUploader
         * - Try to use ChromeDriver as little as possible. Try to make everything using the internet be made with no concrete browser or input sim.
         * - Maybe find a way to compile ffmpeg.exe as a smaller file with only the needed components.
         * - Document code cause its kind of a mess at the moment. Especially everything related to ManualResetEvents.
+        * - Reorganize classes. Too many functions in Program.cs.
+        * - Eventually make a GUI.
         */
 
 
@@ -95,19 +99,29 @@ namespace MusicUploader
         {
             if (File.Exists(Environment.CurrentDirectory + SAVE_FILE))
             {
+                string url = File.ReadAllText(Environment.CurrentDirectory + SAVE_FILE);
+                try
+                {
+                    Task.WaitAll(ValidateUrlAsync(url));
+                } catch (Exception e)
+                {
+                    return changeUrl();
+                }
+
+
                 while (true)
                 {
                     try
                     {
-                    Console.WriteLine(CHANGE_URL_MESSAGE);
-                    ConsoleKeyInfo ans = Console.ReadKey();
-                    Console.WriteLine();
-                    if (ans.Key == ConsoleKey.Y)
-                        return changeUrl();
-                    else if (ans.Key == ConsoleKey.N)
-                        return File.ReadAllText(Environment.CurrentDirectory + SAVE_FILE);
-                    else
-                        throw new InvalidInputException(ans.Key.ToString());
+                        Console.WriteLine(CHANGE_URL_MESSAGE);
+                        ConsoleKeyInfo ans = Console.ReadKey();
+                        Console.WriteLine();
+                        if (ans.Key == ConsoleKey.Y)
+                            return changeUrl();
+                        else if (ans.Key == ConsoleKey.N)
+                            return url;
+                        else
+                            throw new InvalidInputException(ans.Key.ToString());
                     }
                     catch (Exception e)
                     {
@@ -121,25 +135,29 @@ namespace MusicUploader
             }
         }
 
+        public static async Task ValidateUrlAsync(string youtubeUrl)
+        {
+            string url = VALIDATOR_URL + youtubeUrl;
+            Uri uri = new Uri(url, UriKind.Absolute);
+            HttpClient client = new HttpClient();
+            HttpStatusCode status = (await client.GetAsync(uri)).StatusCode;
+            if (status != System.Net.HttpStatusCode.OK)
+                throw new InvalidInputException(youtubeUrl);
+        }
+
         //Validation done for now.
         public static string changeUrl()
         {
             while (true)
                 {
+                    Console.WriteLine(URL_PROMPT);
+                    string url = Console.ReadLine();
                     try
                     {
-                        Console.WriteLine(URL_PROMPT);
-                        string url = Console.ReadLine();
-                        Match match = URL_REGEX.Match(url);
-                        if (match.Success)
-                        {
-                            File.WriteAllText(Environment.CurrentDirectory + SAVE_FILE, url);
-                            return url;
-                        }
-                        else
-                        {
-                            throw new InvalidInputException(url);
-                        }
+                        Task.WaitAll(ValidateUrlAsync(url));
+
+                        File.WriteAllText(Environment.CurrentDirectory + SAVE_FILE, url);
+                        return url;
                     }
                     catch (Exception e)
                     {
