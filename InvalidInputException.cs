d58ab916325afc8d3949b6e6a36b50adedd52e01
mikee31 <113145019+mikee31@users.noreply.github.com>
2022-12-26 10:05:42 -0500

    Delete InvalidInputException.cs

--- InvalidInputException.cs
+++
@@ -1,9 +0,0 @@
-namespace MusicUploader
-{
-    public class InvalidInputException : Exception
-    {
-        public InvalidInputException(string input)
-            : base($"\"{input}\" is not a valid option.")
-        { }
-    }
-}
